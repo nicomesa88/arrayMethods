bcf0989ee36641a278bd0c56b7d5f2218969f6eb
Nicolas Mesa <nicolasmesa@Nicolass-MacBook-Air.local>
2016-07-07 08:52:40 -0500

    firt commit

---
+++ arrayMethods.js
@@ -0,0 +1,134 @@
+//  Part I
+
+// ----------------------------
+// write your own forEach() function that takes an array and a function
+// ----------------------------
+
+function forEach(array, callback){
+    // YOUR CODE HERE
+    for (var i = 0; i < array.length; i++) {
+        callback(array[i], i)
+   }
+}
+
+// tests
+// ---
+var total = 1
+forEach([1, 2, 3, 4], function(a){ total *= a; })
+console.assert(total === 24)
+
+// ----------------------------
+// write your own reduce()
+// that takes an array and a function
+// ----------------------------
+
+function reduce(array, callback){
+    // YOUR CODE HERE
+    var total = array[0]
+    for (var i = 1; i <array.length; i++){
+    total = callback(total, array[i])
+    }
+    return total
+}
+
+// tests
+// ---
+console.assert(
+    reduce([1, 2, 3, 4], function(accumulator, element){ return accumulator * element }) === 24
+)
+console.assert(
+    reduce([1, 2, 3, 4], function(accumulator, element){ return accumulator + element }) === 10
+)
+
+
+
+// ----------------------------
+// write your own map()
+// that takes an array and a function
+// ----------------------------
+
+function map(array, callback){
+    // YOUR CODE HERE
+    for (var i = 1; i <array.length; i++){
+    array[i] = callback(array[i])
+    }
+    return array
+}
+
+// tests
+// ---
+var squares = map([1, 2, 3, 4], function(v){ return v*v })
+console.assert(squares[0] === 1)
+console.assert(squares[1] === 4)
+console.assert(squares[2] === 9)
+console.assert(squares[3] === 16)
+
+// ----------------------------
+// write your own filter()
+// that takes an array and a function
+// ----------------------------
+
+function filter(array, callback){
+    // YOUR CODE HERE
+    return array.filter(callback)
+}
+
+// tests
+// ---
+var evens = filter([1, 2, 3, 4], function(v){ return v%2 === 0 })
+console.assert(evens[0] === 2)
+console.assert(evens[1] === 4)
+
+
+// ----------------------------
+// using Array.sort(), sort the following array
+// of people by name
+// ----------------------------
+
+var people = [
+    {name:"Matt", alma_mater:"Univ of Texas - Austin"},
+    {name:"Brian", alma_mater:"Texas A&M"},
+    {name:"Jesse", alma_mater:"Univ of Texas - Austin"},
+    {name:"Justin", alma_mater:"Univ of Florida"}
+]
+
+people.sort(function(a, b){
+    // YOUR CODE HERE
+})
+
+// tests
+// ---
+console.assert(people[0].name === "Brian")
+console.assert(people[1].name === "Jesse")
+console.assert(people[2].name === "Justin")
+
+// ----------------------------
+// Using Array.map(), Array.filter(), and Array.sort() on the
+// array below:
+// - filter for customers whose first-names start with 'J',
+// - map to their fullnames,
+// - and then sort the items alphabetically by fullname
+// ----------------------------
+
+var customers = [
+    { first: 'Joe', last: 'Blogs'},
+    { first: 'John', last: 'Smith'},
+    { first: 'Dave', last: 'Jones'},
+    { first: 'Jack', last: 'White'}
+]
+
+var results = customers
+    .filter(function(){
+        // YOUR CODE HERE
+    })
+    .map(function(){
+        // YOUR CODE HERE
+    })
+    .sort(function(){
+        // YOUR CODE HERE
+    })
+
+// tests
+// ---
+console.assert(results[0].fullname === "Jack White")
+console.assert(results[2].fullname === "John Smith")
